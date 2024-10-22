1632fd174a0122c7f7c6045ae7c79f272d2255da
pedrohenri07 <0000111063903xsp@al.educacao.sp.gov.br>
2024-10-22 09:50:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(  
+    'Lingua inglesa',
+    'Como se diz te amo em Inglês?',
+    'Te amo em ingles é I love you'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Canadá?',
+    'A capital do Canadá é Ottawa'
+)
+
+criaCartao(
+    'Ciências',
+    'O que é uma célula?',
+    'Uma célula é a menor unidade existente que compõe um ser vivo'
+)
+
+criaCartao(
+    'Esporte',
+    'Quais são as onze posições dos jogadores no futebol?',
+    'Goleiro; Lateral direito; Lateral esquerdo; Zagueiro; Zagueiro central; Volante; Meio-campista Direito ( Ala ); Meio-campista Centro ( Ala ); Meio-campista ( meia-armador ); Meio-campista ofensivo ( meia-atacante ); Centroavante'
+)
